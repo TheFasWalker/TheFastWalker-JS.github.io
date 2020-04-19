06877c63a28c83c088dffd94dc72339ca704b6d6
TheFastWalker <56650860+TheFastWalker@users.noreply.github.com>
2020-04-19 18:26:12 +0300

    начало работы

---
+++ script.js
@@ -0,0 +1,11 @@
+'use strict'
+$('document').ready(function(){
+    loadGoods()})
+
+
+//загрузка товаров на страницу
+function loadGoods(){
+    $.getJSON('goods.json'), function (data){
+        console.log(data);
+    }
+}
