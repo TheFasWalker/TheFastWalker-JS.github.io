3364d1f61d187e9569a820625f086f51df95d6b6
TheFastWalker <56650860+TheFastWalker@users.noreply.github.com>
2020-04-19 19:18:45 +0300

    правка JS

--- script.js
+++ script.js
@@ -7,6 +7,12 @@ $('document').ready(function(){
 //загрузка товаров на страницу
 function loadGoods() {
     $.getJSON('goods.json', function (data){
-        console.log(data);
+        //console.log(data);
+	    var out = '';
+	    for (var key on data){
+		    out+='<p>'+data[key]['title']+'</p>';
+		    out+='<p>'+data[key]['cost']+'</p>';		    
+	    }
+	    $('#goods').html(out);
     })
 }
